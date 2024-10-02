126179c180b1c5a373e7f21074c5c1c6038bc22e
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2024-10-02 12:02:29 +0200

    discovery: add containerID information to discovered services

--- pkg/collector/corechecks/servicediscovery/impl_linux.go
+++ pkg/collector/corechecks/servicediscovery/impl_linux.go
@@ -15,6 +15,7 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/collector/corechecks/servicediscovery/servicetype"
 	pkgconfigsetup "github.com/DataDog/datadog-agent/pkg/config/setup"
 	processnet "github.com/DataDog/datadog-agent/pkg/process/net"
+	proccontainers "github.com/DataDog/datadog-agent/pkg/process/util/containers"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
@@ -28,8 +29,8 @@ type linuxImpl struct {
 	getSysProbeClient func() (systemProbeClient, error)
 	time              timer
 
-	ignoreCfg map[string]bool
-	store     workloadmeta.Component
+	ignoreCfg         map[string]bool
+	containerProvider proccontainers.ContainerProvider
 
 	ignoreProcs       map[int]bool
 	aliveServices     map[int]*serviceInfo
@@ -41,7 +42,7 @@ func newLinuxImpl(ignoreCfg map[string]bool, store workloadmeta.Component) (osIm
 		getSysProbeClient: getSysProbeClient,
 		time:              realTime{},
 		ignoreCfg:         ignoreCfg,
-		store:             store,
+		containerProvider: proccontainers.GetSharedContainerProvider(store),
 		ignoreProcs:       make(map[int]bool),
 		aliveServices:     make(map[int]*serviceInfo),
 		potentialServices: make(map[int]*serviceInfo),
@@ -88,6 +89,13 @@ func (li *linuxImpl) DiscoverServices() (*discoveredServices, error) {
 	}
 	clear(li.potentialServices)
 
+	// Get container IDs to enrich the service info with it. The SD check is
+	// supposed to run once every minute, so we use this duration for cache
+	// validity.
+	// TODO: use/find a global constant for this delay, to keep in sync with
+	// the check delay if it were to change.
+	containers := li.containerProvider.GetPidToCid(1 * time.Minute)
+
 	// check open ports - these will be potential new services if they are still alive in the next iteration.
 	for _, service := range response.Services {
 		pid := service.PID
@@ -103,6 +111,12 @@ func (li *linuxImpl) DiscoverServices() (*discoveredServices, error) {
 				li.ignoreProcs[pid] = true
 				continue
 			}
+
+			if id, ok := containers[pid]; ok {
+				svc.meta.ContainerID = id
+				log.Debugf("[pid: %d] add containerID to process: %s", pid, id)
+			}
+
 			log.Debugf("[pid: %d] adding process to potential: %s", pid, svc.meta.Name)
 			li.potentialServices[pid] = &svc
 		}
